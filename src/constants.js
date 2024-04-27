5a93fb66bf14394615abbd023f3fedc1ff2b00b8
oussama2505 <oussama.brahmi0608@gmail.com>
2024-04-27 15:35:02 +0200

    Git Upload

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Este es mi PC. Últimamente trabajo principalmente en JavaScript/TypeScript. He creado un par de juegos en ese lenguaje. También me gusta Python. De todos modos, independientemente del lenguaje, simplemente me gusta programar. ¡Aquí está mi <a href="https://github.com/oussama2505?tab=stars" target="_blank">Github</a>!`,
+  "cs-degree": ` Este es mi título en Ciencias de la Computación. Lo colgué en la pared porque estoy orgulloso de él. Fue un grado muy teórico, pero creo que me dio una buena base.`,
+  "sofa-table": `Ese es mi sofá. Me gusta relajarme aquí y ver YouTube.¡Ve a ver <a href="https://www.youtube.com/watch?v=wy_fSStEgMs" target="_blank">este video! </a>
+  Aprenderás cómo construir un portafolio como el que estás experimentando actualmente.`,
+  tv: ` Esa es mi TV. Últimamente he estado viendo mucho a youtubers tecnológicos como:
+   <a href="https://www.youtube.com/@freecodecamp" target="_blank">FreeCodeCamp</a>, <a href="https://www.youtube.com/@HolaMundoDev" target="_blank">HolaMundoDev</a>,
+  <a href="https://www.youtube.com/@ByteGrad" target="_blank">ByteGrad</a> (sometimes) and <a href="https://www.youtube.com/@mouredev" target="_blank">mouredev</a>!`,
+  bed: `Aquí es donde duermo. Las grandes ideas vienen cuando estoy acostado en mi cama. Cuando surge una idea, a menudo tengo que escribirla o de lo contrario no podré dormir porque mi energía mental está consumida por ella.`,
+  resume: ` Este es mi escritorio y sobre él está mi currículum. <a href="https://github.com/oussama2505/CV/blob/main/Oussama_brahmi_1.pdf" target="_blank">¿Echar un vistazo? </a>
+  Contáctame en oussama.brahmi0608@gmail.com si tienes alguna oportunidad de trabajo interesante.
+`,
+  projects: `Información sobre este portafolio: Está hecho con la biblioteca Kaboom.js, que es una biblioteca para hacer juegos en JavaScript. El texto se renderiza con HTML/CSS. Entonces, el cuadro de texto que estás leyendo actualmente no se renderiza dentro del canvas. Aprende más sobre cómo usar Kaboom.js viendo algunos de estos tutoriales aquí. <a href="https://youtube.com/@jslegenddev" target="_blank">Aquí</a>.`,
+  library: `Hay muchos libros de programación en mis estantes. Incluso hay uno en francés (también hablo francés, por cierto). Probablemente solo leí uno de ellos. ¿Quién más compra compulsivamente libros técnicos sin terminarlos nunca?`,
+  exit: `Si quieres salir del portafolio de Oussama, simplemente cierra la pestaña.`,
+};
